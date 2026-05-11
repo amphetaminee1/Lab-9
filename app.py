87a60ab07295b8ec32550d614475c93e0c984e13
Афнфиса Вартыкян <gaarasabakyno83@gmail.com>
2026-05-11 12:52:44 +0300

    Реализация доп задания

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500))
     rate = db.Column(db.Integer)
+    is_visible = db.Column(db.Boolean, default=True)
 
 
 with app.app_context():
@@ -20,18 +21,26 @@ with app.app_context():
 def index():
     if request.method == 'POST':
         t = request.form['text']
-        r = request.form['rate']
+        r = request.form.get('rate')
+        if not r: r = 5
 
-        if r == "":
-            r = 5
-
-        new_obj = Review(text=t, rate=int(r))
+        new_obj = Review(text=t, rate=int(r), is_visible=True)
         db.session.add(new_obj)
         db.session.commit()
         return redirect('/')
 
-    all_data = Review.query.all()
-    return render_template('task.html', reviews=all_data)
+    visible_data = Review.query.filter_by(is_visible=True).all()
+    return render_template('task.html', reviews=visible_data)
+
+
+@app.route('/clear', methods=['POST'])
+def clear_view():
+    visible_reviews = Review.query.filter_by(is_visible=True).all()
+    for r in visible_reviews:
+        r.is_visible = False
+
+    db.session.commit()
+    return redirect('/')
 
 
 if __name__ == '__main__':
